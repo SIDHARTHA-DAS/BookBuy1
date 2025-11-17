40dc668526a34d7ce572b516b0f07cfd494c8a53
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2025-11-17 23:18:54 +0530

    add

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-black">
+    <div className="h-screen w-full flex justify-center items-center bg-gray-900">
       <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-blue-500"></div>
     </div>
   );
